cd2b5c61dfda165104d63ddd478db8fceee9a346
swapnil-gadekar <swapnil.gdkr96@gmail.com>
2020-05-24 19:10:51 +0530

    Updates

---
+++ precache-manifest.7c7d2f912d9380ecfc25c9f21bff7acd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce907ba5d82f597f0b912361995a986",
+    "url": "/unique-clothing/index.html"
+  },
+  {
+    "revision": "92c42fac3ae705ce0aff",
+    "url": "/unique-clothing/static/css/main.24d136a6.chunk.css"
+  },
+  {
+    "revision": "5518d9e44e4327499395",
+    "url": "/unique-clothing/static/js/2.e30a232f.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/unique-clothing/static/js/2.e30a232f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92c42fac3ae705ce0aff",
+    "url": "/unique-clothing/static/js/main.6ce6365d.chunk.js"
+  },
+  {
+    "revision": "2ff3936f9395cad24af6",
+    "url": "/unique-clothing/static/js/runtime-main.50932777.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/unique-clothing/static/media/cart.30f07071.svg"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/unique-clothing/static/media/logo.ed28accb.svg"
+  }
+]);
